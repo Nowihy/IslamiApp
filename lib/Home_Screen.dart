92fc47ea89532da4daec731ba0b4d016d0932566
Nowihy <khaledelnowihy11@gmail.com>
2022-08-01 22:39:50 +0200

    Add Splash Screen and NavBar

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/Hadeth_Tab.dart';
+import 'package:islami_app/MyColors.dart';
+import 'package:islami_app/Quran_Tab.dart';
+import 'package:islami_app/Radio_Tab.dart';
+import 'package:islami_app/Sebha_Tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  static const String routeName = 'HomeScreen';
 
-  static const String routeName = 'HomeScreen' ;
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Islami App'),
-      ),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/background.png'))),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            centerTitle: true,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+                color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),
+            title: Text('Islami'),
+          ),
+          bottomNavigationBar: Theme(
+            data:
+                Theme.of(context).copyWith(canvasColor: MyColors.primaryColor),
+            child: BottomNavigationBar(
+              currentIndex: selectedIndex,
+              onTap: (index) {
+                selectedIndex = index;
+                setState(() {});
+              },
+              selectedIconTheme: IconThemeData(color: Colors.black),
+              unselectedIconTheme: IconThemeData(color: Colors.white),
+              items: [
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/quran.png')),
+                    label: 'Quran'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/hadeth.png')),
+                    label: 'Hadeth'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/sebha.png')),
+                    label: 'Sebha'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/radio.png')),
+                    label: 'Radio'),
+              ],
+            ),
+          ),
+          body: tabs[selectedIndex]),
     );
   }
+
+  List<Widget> tabs = [QuranTab(), HadethTab(), SebhaTab(), RadioTab()];
 }
---
+++ lib/MyColors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static final Color primaryColor = Color(0xffB7935F);
+}
